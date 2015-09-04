713b3c8e3d0be10bf3d137f4e3eec569f2e3be65
mikelovskij <snao20@hotmail.com>
2015-09-04 18:36:41 +0200

    test code for nds2 iterators

---
+++ ndstest.py
@@ -0,0 +1,39 @@
+import nds2
+
+conn = nds2.connection('nds.ligo-la.caltech.edu')
+print conn
+channel = ['L1:GDS-CALIB_STRAIN']
+print "channel = {0}".format(channel)
+
+print "Now testing the command 'conn.iterate(channel)' "
+
+try:
+    iter = conn.iterate(channel)
+except RuntimeError, err:
+    print "Failed"
+    print RuntimeError, err
+
+
+print "Now testing the command 'conn.iterate(1, channel)' "
+
+try:
+    iter = conn.iterate(1, channel)
+except RuntimeError, err:
+    print "Failed"
+    print RuntimeError, err
+
+print "Now testing the command 'conn.iterate(1125417950, 1125418050, channel)' "
+
+try:
+    iter = conn.iterate(1125417950, 1125418050, channel)
+except RuntimeError, err:
+    print "Failed"
+    print RuntimeError, err
+
+print "Now testing the command 'conn.iterate(1125417950, 1125418050, 1, channel)' "
+
+try:
+    iter = conn.iterate(1125417950, 1125418050, 1, channel)
+except RuntimeError, err:
+    print "Failed"
+    print RuntimeError, err
